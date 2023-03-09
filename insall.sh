ab21ce43e03876a3210a33c495f6ea8751f6db2f
kohta ito <kohta110@gmail.com>
2023-03-09 18:59:57 +0900

    update

--- insall.sh
+++ insall.sh
@@ -2,10 +2,10 @@
 
 set -exuo pipefail
 
-nodejs_version="18.0.0"
+nodejs_version="18.15.0"
 
-# http://nodejs.org/dist/v${nodejs_version}/SHASUMS256.txt
-nodejs_checksum="6260d3526dff25d43451ea8e90e0174975b4cd067e8535dc1d85a6d6b29f3043"
+# https://nodejs.org/dist/v${nodejs_version}/SHASUMS256.txt
+nodejs_checksum="b298a73a9fc07badfa9e4a2e86ed48824fc9201327cdc43e3f3f58b273c535e7"
 
 nodejs_url="http://nodejs.org/dist/v${nodejs_version}/node-v${nodejs_version}-linux-x64.tar.gz"
 
