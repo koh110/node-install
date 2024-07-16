a45f927f19fd4f4420cf72dbdee9dda9a37e9c09
kohta ito <kohta110@gmail.com>
2024-07-16 13:20:30 +0900

    bump

--- insall.sh
+++ insall.sh
@@ -2,10 +2,10 @@
 
 set -exuo pipefail
 
-nodejs_version="22.2.0"
+nodejs_version="22.4.1"
 
 # https://nodejs.org/dist/v${nodejs_version}/SHASUMS256.txt
-nodejs_checksum="2c6eaf8bfd0f886ed8764ffce19c795e909639105e4056b1d9f8f917bad12cf1"
+nodejs_checksum="addb41bd7d5bdef51dc3bd76292889692664b3c9de1b7c5f89aba95e474aee84"
 
 nodejs_url="https://nodejs.org/dist/v${nodejs_version}/node-v${nodejs_version}-linux-x64.tar.gz"
 
