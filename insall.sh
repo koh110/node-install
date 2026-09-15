cb9ff8a054434ff976239efb67e680652db1ba3c
kohta ito <kohta110@gmail.com>
2026-09-15 15:24:55 +0900

    update

--- insall.sh
+++ insall.sh
@@ -2,10 +2,10 @@
 
 set -exuo pipefail
 
-nodejs_version="26.5.0"
+nodejs_version="26.8.2"
 
 # https://nodejs.org/dist/v${nodejs_version}/SHASUMS256.txt
-nodejs_checksum="22b5f47ad6ae78837e4c2b846019965ce1a06ba143de176102294a1bf44fc677"
+nodejs_checksum="badb3fe6a61b85e1352ca6564dc56b8b7bd5ecd5c474c52acc21dd0cdd586f35"
 
 nodejs_url="https://nodejs.org/dist/v${nodejs_version}/node-v${nodejs_version}-linux-x64.tar.gz"
 
